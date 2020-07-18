4973a380efa8f3f921bca031b8e98b344574e95e
masterugwee <varun199700@gmail.com>
2020-07-18 19:40:25 +0530

    Password Validator class , for checking the passwords entered by the user.

--- app/src/main/java/com/example/passwordmanager/PasswordValidator.java
+++ app/src/main/java/com/example/passwordmanager/PasswordValidator.java
@@ -25,16 +25,20 @@ public class PasswordValidator {
         rules.add(new CharacterRule(EnglishCharacterData.Digit,1));
         //Rule to check for special characters
         rules.add(new CharacterRule(EnglishCharacterData.Special,1));
+        //Adding the rules to validator
         org.passay.PasswordValidator validator = new org.passay.PasswordValidator(rules);
+        //taking in the userpassword
         PasswordData password = new PasswordData(userpassword);
+        //Generating a result
         RuleResult resut = validator.validate(password);
+        //Checking is result is valid or not.
         if(resut.isValid())
         {
             return "Password Validated";
         }
         else
             return "Invalid Password "+validator.getMessages(resut);
-
+        //returning the invalid password messages.
     }
 
 
