e82570c64b7f05db8daeb85075d99218247a684e
masterugwee <varun199700@gmail.com>
2020-07-18 20:23:13 +0530

    Password Validator class , for checking the passwords entered by the user.

--- app/src/main/java/com/example/passwordmanager/CreatePassword.java
+++ app/src/main/java/com/example/passwordmanager/CreatePassword.java
@@ -3,12 +3,18 @@ import org.passay.*;
 public class CreatePassword {
 
     public String createpassword() {
+//Methord to create password.
 
-        CharacterRule alphabets = new CharacterRule(EnglishCharacterData.Alphabetical);
-        CharacterRule digits = new CharacterRule(EnglishCharacterData.Digit);
-        CharacterRule special = new CharacterRule(EnglishCharacterData.Special);
+        CharacterRule alphabets = new CharacterRule(EnglishCharacterData.Alphabetical);   //Alphabets
+
+        CharacterRule digits = new CharacterRule(EnglishCharacterData.Digit);   //Digits
+
+        CharacterRule special = new CharacterRule(EnglishCharacterData.Special); //Special characters
+
         PasswordGenerator passwordGenerator = new PasswordGenerator();
-        String password = passwordGenerator.generatePassword(16, alphabets, digits, special);
+
+        String password = passwordGenerator.generatePassword(16, alphabets, digits, special);  //Password generation.
+
         return password;
     }
 
