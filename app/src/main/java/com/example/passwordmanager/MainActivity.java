b9f28f9b36945521258450a7d5ab5fb1aea9fb63
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-07-18 06:22:05 +0530

    Intent added for Login and Signup pages

--- app/src/main/java/com/example/passwordmanager/MainActivity.java
+++ app/src/main/java/com/example/passwordmanager/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.passwordmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login, signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = (Button)findViewById(R.id.loginMainButton);
+        signup = (Button)findViewById(R.id.signUpMain);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
