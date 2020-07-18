b95aa18359ae51125ca66631ff34f5416934750b
masterugwee <varun199700@gmail.com>
2020-07-18 21:35:13 +0530

    Moved creation and validation to one class.

--- app/src/main/java/com/example/passwordmanager/CreateValidatePassword.java
+++ app/src/main/java/com/example/passwordmanager/CreateValidatePassword.java
@@ -4,8 +4,23 @@ import org.passay.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PasswordValidator {
+public class CreateValidatePassword {
 
+    public String createpassword() {
+//Methord to create password.
+
+        CharacterRule alphabets = new CharacterRule(EnglishCharacterData.Alphabetical);   //Alphabets
+
+        CharacterRule digits = new CharacterRule(EnglishCharacterData.Digit);   //Digits
+
+        CharacterRule special = new CharacterRule(EnglishCharacterData.Special); //Special characters
+
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+
+        String password = passwordGenerator.generatePassword(16, alphabets, digits, special);  //Password generation.
+
+        return password;
+    }
 
     public String Validator(String userpassword)
     {
@@ -40,7 +55,5 @@ public class PasswordValidator {
             return "Invalid Password "+validator.getMessages(resut);
         //returning the invalid password messages.
     }
-
-
 
 }
