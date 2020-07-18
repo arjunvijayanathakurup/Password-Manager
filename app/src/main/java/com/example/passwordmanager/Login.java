97eeba66475f392a25888dadbe9ca3ad6599151f
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-07-18 07:47:49 +0530

    added Login and signup fields verifications

--- app/src/main/java/com/example/passwordmanager/Login.java
+++ app/src/main/java/com/example/passwordmanager/Login.java
@@ -3,12 +3,36 @@ package com.example.passwordmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
+    private EditText signedUpEmail, signedUpPassword;
+    Button signupButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signedUpEmail = (EditText)findViewById(R.id.signupEmail);
+        signedUpPassword = (EditText)findViewById(R.id.signupPassword);
+        signupButton = (Button)findViewById(R.id.signupButton);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(signedUpEmail.getText().toString())) {
+                    signedUpEmail.setError("Please enter an Email id");
+                    return;
+                }
+                if(TextUtils.isEmpty(signedUpPassword.getText().toString())) {
+                    signedUpPassword.setError("Please enter an Email id");
+                    return;
+                }
+                
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/passwordmanager/SignUp.java
+++ app/src/main/java/com/example/passwordmanager/SignUp.java
@@ -2,13 +2,54 @@ package com.example.passwordmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
-
+    EditText signUpEmail, signUpPassword, signUpConfirm, masterPasswordHint;
+    Button createAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        signUpEmail = (EditText)findViewById(R.id.signupEmail);
+        signUpPassword = (EditText)findViewById(R.id.signupPassword);
+        signUpConfirm = (EditText)findViewById(R.id.signupPasswordConfirm);
+        masterPasswordHint = (EditText)findViewById(R.id.masterPasswordHint);
+        createAccount = (Button)findViewById(R.id.signupButton);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(signUpEmail.getText().toString())) {
+                    signUpEmail.setError("Please enter an Email id");
+                    return;
+                }
+                if(TextUtils.isEmpty(signUpPassword.getText().toString())) {
+                    signUpPassword.setError("Please enter a Password");
+                    return;
+                }
+                if(TextUtils.isEmpty(signUpConfirm.getText().toString())) {
+                    signUpConfirm.setError("Please confirm the entered Password");
+                    return;
+                }
+                if(signUpEmail.getText().toString() != null && signUpPassword.getText().toString() != null && signUpPassword.getText().toString().equals(signUpConfirm.getText().toString())){
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(getApplicationContext(), "Signed Up, Please Login with the credentials", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    if(!signUpPassword.getText().toString().equals(signUpConfirm.getText().toString())) {
+                        Toast.makeText(getApplicationContext(), "Passwords do not match.", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
     }
 }
