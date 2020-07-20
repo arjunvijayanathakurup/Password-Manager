1d16463883b8a5c47d2255c66d7311621f431e77
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-07-20 14:06:31 +0530

    Updated Database connectivity and password validation

--- app/src/main/java/com/example/passwordmanager/Login.java
+++ app/src/main/java/com/example/passwordmanager/Login.java
@@ -1,8 +1,12 @@
 package com.example.passwordmanager;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,7 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Login extends AppCompatActivity {
     private EditText signedUpEmail, signedUpPassword;
     Button signupButton;
-    LoginDatabase loginDatabase;
+    LoginHelper loginDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,32 +23,36 @@ public class Login extends AppCompatActivity {
         signedUpEmail = (EditText)findViewById(R.id.signupEmail);
         signedUpPassword = (EditText)findViewById(R.id.signupPassword);
         signupButton = (Button)findViewById(R.id.signupButton);
+        loginDatabase = new LoginHelper(this);
 
         signupButton.setOnClickListener(view -> {
             if(TextUtils.isEmpty(signedUpEmail.getText().toString())) {
                 signedUpEmail.setError("Please enter an Email id");
-
             }
             else if(TextUtils.isEmpty(signedUpPassword.getText().toString())) {
-                signedUpPassword.setError("Please enter an Email id");
+                signedUpPassword.setError("Please enter a password");
             }
             else
             {
-                String userName=String.valueOf(signedUpEmail);
-                String password=String.valueOf(signedUpPassword);
-                String storedpassword = loginDatabase.getSinlgeEntry(userName);
-                if(password.equals(storedpassword))
+                String userName = signedUpEmail.getText().toString();
+                String password = signedUpPassword.getText().toString();
+                Cursor storedpassword = loginDatabase.getData(userName);
+                storedpassword.moveToFirst();
+                String resEmail = storedpassword.getString(storedpassword.getColumnIndex(LoginHelper.CONTACTS_COLUMN_EMAIL));
+                String resPassword = storedpassword.getString(storedpassword.getColumnIndex(LoginHelper.CONTACTS_COLUMN_PASSWORD));
+
+                if(resEmail.equals(userName) && resPassword.equals(password))
                 {
-                    Toast.makeText(Login.this, "Congrats: Login Successfull", Toast.LENGTH_LONG).show();
+                    Toast.makeText(Login.this, "Congrats: Login Successful", Toast.LENGTH_LONG).show();
                     //Dialoge dismiss
+                    Intent intent = new Intent(getApplicationContext(), MainLayout.class);
+                    startActivity(intent);
                 }
                 else
                 {
                     Toast.makeText(Login.this, "User Name or Password does not match", Toast.LENGTH_LONG).show();
                 }
             }
-
-
         });
 
     }
