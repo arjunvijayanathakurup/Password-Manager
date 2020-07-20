65077485a5fb5eb12a5671a04d2eddd89fca67c7
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-07-20 22:12:50 +0530

    Updated MainLayout file with PasswordAdapter

--- app/src/main/java/com/example/passwordmanager/MainLayout.java
+++ app/src/main/java/com/example/passwordmanager/MainLayout.java
@@ -2,20 +2,32 @@ package com.example.passwordmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainLayout extends AppCompatActivity {
     ListView simpleList;
-    String animalList[] = {"Lion","Tiger","Monkey","Elephant","Dog","Cat","Camel"};
+    DBHelper dbHelper;
+    private ArrayAdapter arrayAdapter;
+    private ArrayList arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_layout);
         simpleList = (ListView) findViewById(R.id.listView);
+        dbHelper = new DBHelper(this);
+        arrayList = dbHelper.getAllData();
+        if(arrayList == null){
+            Intent intent = new Intent(getApplicationContext(), AddPassword.class);
+            startActivity(intent);
+        }else{
+            arrayAdapter = new ArrayAdapter(this, R.layout.activity_list_layout, arrayList);
+            simpleList.setAdapter(arrayAdapter);
+        }
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_list_layout, R.id.name, animalList);
-        simpleList.setAdapter(arrayAdapter);
     }
 }
