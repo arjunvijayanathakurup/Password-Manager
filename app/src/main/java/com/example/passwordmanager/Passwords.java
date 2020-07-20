41d700b1371629b432f1383d8744a654a5ca884d
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-07-20 22:13:51 +0530

    Added Passwords model

--- app/src/main/java/com/example/passwordmanager/Passwords.java
+++ app/src/main/java/com/example/passwordmanager/Passwords.java
@@ -14,4 +14,44 @@ public class Passwords {
         this.url = url;
         this.username = username;
     }
+
+    public String getMailId() {
+        return mailId;
+    }
+
+    public void setMailId(String mailId) {
+        this.mailId = mailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
